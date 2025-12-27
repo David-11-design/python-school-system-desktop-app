4168d1d677c81e14b5c2d03bdad0ee532abb640a
David-11-design <arevaloandy073@gmail.com>
2025-12-27 14:35:42 -0500

    In DB Layer I created the tables students, courses, subjects and notes

--- School-System/db/db_conection.py
+++ School-System/db/db_conection.py
@@ -19,6 +19,30 @@ try:
                    fullname TEXT NOT NULL,
                    username TEXT NOT NULL,
                    password TEXT NOT NULL)''')
+    
+    cursor.execute('''CREATE TABLE IF NOT EXISTS students(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   name TEXT NOT NULL,
+                   fullname TEXT NOT NULL,
+                   age INTEGER NOT NULL)''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS courses(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   name TEXT NOT NULL,
+                   parallel TEXT NOT NULL)''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS subjects(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   name TEXT NOT NULL)''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS notes(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   student_id INTEGER NOT NULL,
+                   subject_id INTEGER NOT NULL,
+                   note REAL NOT NULL,
+                   FOREIGN KEY(student_id) REFERENCES students(id),
+                   FOREIGN KEY(subject_id) REFERENCES subjects(id))''')
+
     conexion.commit()
 
 except sqlite3.Error as e:
